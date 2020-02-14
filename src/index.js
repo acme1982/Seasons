e50b49c9eb49807e3740990a17ca5d0beb96a08c
Marius <marius.nalivaika@gmail.com>
2020-02-14 11:05:04 +0000

    Created useLocation for code reusability, moved getLocation out of component.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
 const App = () => {
-	const [lat, setLat] = useState(null);
-	const [errorMsg, setErrorMsg] = useState('');
-
-	useEffect(() => {
-		window.navigator.geolocation.getCurrentPosition(
-			position => setLat(position.coords.latitude),
-			err => setErrorMsg(err.message)
-		);
-	}, []);
+	const { lat, errorMsg } = useLocation();
 
 	let content;
 	if (errorMsg) {
